7d2740e7a090b13e33818cb3a20eb2adb4c897c0
Vlad Patrushev <patr982@gmail.com>
2022-04-11 10:38:01 +0000

    refactor

--- src/data/datasets/representation/unsupervised_contrastive_dataset.py
+++ src/data/datasets/representation/unsupervised_contrastive_dataset.py
@@ -1,16 +1,23 @@
 from typing import Union, Optional
 
 import torch
+import pandas as pd
 from albumentations import BasicTransform
 from albumentations.core.composition import BaseCompose
 
-from src.registry import DATASETS
 from src.data.datasets.base import ImageDataset
 
 
-@DATASETS.register_class
 class UnsupervisedContrastiveDataset(ImageDataset):
-    """A generic dataset for image contrastive task"""
+    """
+    A dataset for unsupervised contrastive task
+    (one image is transformed twice so that they are positive to each other)
+
+    .. csv-table:: UnsupervisedContrastive csv example
+     :header: image_path
+     cat_1.jpg
+     dog_1.jpg
+    """
 
     def __init__(self,
                  data_folder: str,
@@ -24,7 +31,7 @@ class UnsupervisedContrastiveDataset(ImageDataset):
         Args:
             data_folder: Directory with all the images.
             csv_path: Path to the csv file with path to images and annotations.
-                Path to images must be under column `image_path` and annotations must be under `label` column.
+                Path to images must be under column `input_column`.
             transform: Transform to be applied on a sample. This should have the
                 interface of transforms in `albumentations` library.
             augment: Optional augment to be applied on a sample.
@@ -34,24 +41,26 @@ class UnsupervisedContrastiveDataset(ImageDataset):
             grayscale: if True image will be read as grayscale otherwise as RGB.
 
         """
-        super().__init__(data_folder, csv_path, transform, augment, input_dtype, input_column, grayscale)
+        super().__init__(data_folder, transform, augment, input_dtype, input_column, grayscale)
+        self.__csv_path = csv_path
+        self.__csv = pd.read_csv(self.data_folder / self.__csv_path, dtype={self.input_column: 'str'})
 
     def __getitem__(self, idx: int) -> dict:
-        record = self.csv.iloc[idx]
+        record = self.__csv.iloc[idx]
         image_path = record[self.input_column]
         image = self._read_image(image_path)
-        sample = {'input': image}
+        sample = {'image': image}
 
-        sample_0_transformed = self._apply_transform(self.augment, sample)['input']
-        sample_1_transformed = self._apply_transform(self.augment, sample)['input']
+        sample_0_transformed = self._apply_transform(self.augment, sample)['image']
+        sample_1_transformed = self._apply_transform(self.augment, sample)['image']
 
-        sample_0_augmented = self._apply_transform(self.transform, {'input': sample_0_transformed})
-        sample_1_augmented = self._apply_transform(self.transform, {'input': sample_1_transformed})
+        sample_0_augmented = self._apply_transform(self.transform, {'image': sample_0_transformed})
+        sample_1_augmented = self._apply_transform(self.transform, {'image': sample_1_transformed})
 
-        sample_0 = sample_0_augmented['input'].type(torch.__dict__[self.input_dtype])
-        sample_1 = sample_1_augmented['input'].type(torch.__dict__[self.input_dtype])
+        sample_0 = sample_0_augmented['image'].type(torch.__dict__[self.input_dtype])
+        sample_1 = sample_1_augmented['image'].type(torch.__dict__[self.input_dtype])
 
-        return {'input_0': sample_0, 'input_1': sample_1, 'index': idx}
+        return {'image_0': sample_0, 'image_1': sample_1, 'index': idx}
 
     def __len__(self) -> int:
-        return len(self.csv)
+        return len(self.__csv)
