19a33ec9dd021eb0c9eee113aec5f0f5890b1de7
Vlad Vinogradov <vladt9@yandex.ru>
2022-05-01 22:22:17 +0400

    Added registries for basic components

--- src/constructor/__init__.py
+++ src/constructor/__init__.py
@@ -1,4 +1,8 @@
 from .registry import Registry
 
+
+DATASETS = Registry('datasets')
+TRANSFORMS = Registry('transforms')
 OPTIMIZERS = Registry('optimizers')
 SCHEDULERS = Registry('schedulers')
+LOSSES = Registry('losses')
