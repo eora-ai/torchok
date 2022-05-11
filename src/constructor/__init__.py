3cfbabb92daf36029b3a265bffeba48885a1d66c
VladislavPatrushev <patr982@gmail.com>
2022-05-11 06:12:14 +0000

    change consturctor init

--- src/constructor/__init__.py
+++ src/constructor/__init__.py
@@ -1,5 +1,10 @@
 from .registry import Registry
 
+
+DATASETS = Registry('datasets')
+TRANSFORMS = Registry('transforms')
 OPTIMIZERS = Registry('optimizers')
 SCHEDULERS = Registry('schedulers')
+LOSSES = Registry('losses')
+METRICS = Registry('metrics')
 BACKBONES = Registry('backbones')
