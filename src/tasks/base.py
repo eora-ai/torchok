f14ba04318db0dd42ec00a6291ce0cca67ea690e
VladislavPatrushev <patr982@gmail.com>
2022-05-11 10:51:30 +0000

    base task refactir

--- src/tasks/base.py
+++ src/tasks/base.py
@@ -1,40 +1,29 @@
-from functools import partial
+from typing import Tuple, List, Optional
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 
 import torch
 from pytorch_lightning import LightningModule
-from torch.utils.data import DataLoader
-
-from src.constructor import create_scheduler, create_optimizer, create_dataset, JointLoss
-from src.constructor.config_structure import TrainConfigParams
-from src.metrics import MetricManager
-from src.models.backbones.utils import load_checkpoint
+from omegaconf import DictConfig
 
 
-@dataclass
-class BaseTaskParams:
-    input_shapes: dict
-    checkpoint: str = None
-    onnx_params: dict = None
-    inference_mode: bool = False
+from src.constructor.constuctor import Constructor
 
 
 class BaseTask(LightningModule, ABC):
-    """An abstract class that represent main methods of tasks"""
+    """An abstract class that represent main methods of tasks."""
 
-    def __init__(self, hparams: TrainConfigParams):
-        """
+    def __init__(self, hparams: DictConfig):
+        """Init BaseTask.
+
         Args:
             hparams: Hyperparameters that set in yaml file.
         """
         super().__init__()
-        self.save_hyperparameters(hparams)
-        self.__params = self.config_parser(**hparams.task.params)
-        self.__metric_manager = MetricManager(self.hparams.metrics)
-        self._criterion = JointLoss(self, self.hparams.loss)
-        self.__input_shapes = self.params.input_shapes
-        self.__inference_mode = self.params.inference_mode
+        self.__constructor = Constructor(hparams)
+        self._metric_manager = self.__constructor.configure_metrics_manager()
+        self._criterion = self.__constructor.configure_losses()
+        self.__hparams = self.__constructor.hparams
+        self.__input_shapes = self.__hparams.input_shapes
         self.example_input_array = [torch.rand(1, *shape) for _, shape in self.__input_shapes.items()]
 
     @abstractmethod
@@ -58,105 +47,91 @@ class BaseTask(LightningModule, ABC):
         pass
 
     def on_train_start(self) -> None:
-        if self.params.checkpoint is not None:
-            load_checkpoint(self, self.params.checkpoint, strict=False)
+        # TODO check and load checkpoint
+        pass
 
     def on_test_start(self) -> None:
-        if self.params.checkpoint is not None:
-            load_checkpoint(self, self.params.checkpoint, strict=False)
+        # TODO check and load checkpoint
+        pass
 
     def training_epoch_end(self, outputs: torch.tensor) -> None:
+        """Is calling at the end of the training epoch with the outputs of all training steps."""
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
         self.log('train/loss', avg_loss, on_step=False, on_epoch=True)
         self.log('step', self.current_epoch, on_step=False, on_epoch=True)
         self.log_dict(self.__metric_manager.on_epoch_end('train'))
 
     def validation_epoch_end(self, outputs: torch.tensor) -> None:
+        """Is calling at the end of the validation epoch with the outputs of all validation steps."""
         avg_loss = torch.stack(outputs).mean()
         self.log('valid/loss', avg_loss, on_step=False, on_epoch=True)
         self.log('step', self.current_epoch, on_step=False, on_epoch=True)
         self.log_dict(self.__metric_manager.on_epoch_end('valid'))
 
     def test_epoch_end(self, outputs: torch.tensor) -> None:
+        """Is calling at the end of a test epoch with the output of all test steps."""
         avg_loss = torch.stack(outputs).mean()
         self.log('test/loss', avg_loss, on_step=False, on_epoch=True)
         self.log_dict(self.__metric_manager.on_epoch_end('test'))
 
-    def to_onnx(self) -> None:
+    def to_onnx(self, onnx_params) -> None:
+        """Is saving the model in ONNX format."""
         super().to_onnx(input_sample=self.example_input_array,
-                        **self.params.onnx_params)
+                        **onnx_params)
 
     def on_train_end(self) -> None:
-        if self.params.onnx_params is not None:
-            self.to_onnx()
+        """Is calling at the end of training before logger experiment is closed."""
+        onnx_params = self.__hparams.onnx_params
+        if onnx_params is not None:
+            self.to_onnx(onnx_params)
 
-    def configure_optimizers(self):
-        optimizer = create_optimizer(self.parameters(), self.hparams.optimizers)
-        if self.hparams.schedulers is not None:
-            scheduler = create_scheduler(optimizer, self.hparams.schedulers)
-            return [optimizer], [scheduler]
-        else:
-            return [optimizer]
+    def configure_optimizers(self) -> Tuple[List, List]:
+        """Configure optimizers."""
+        configure_optimizers = self.__constructor.configure_optimizers(self.parameters())
+        optimizers, schedulers = [], []
 
-    @staticmethod
-    def prepare_dataloader(dataset_params, common_dataset_params, dataloader_params) -> torch.DataLoader:
-        dataset = create_dataset(dataset_params.name, common_dataset_params, dataset_params)
+        for item in configure_optimizers:
+            optimizers.append(item['optimizer'])
+            lr_scheduler = item.get('lr_scheduler')
+            if lr_scheduler is not None:
+                schedulers.append(lr_scheduler['scheduler'])
+            else:
+                schedulers.append(None)
+        return optimizers, schedulers
 
-        use_custom_collate_fn = dataloader_params.use_custom_collate_fn and hasattr(dataset, 'collate_fn')
-        collate_fn = dataset.collate_fn if use_custom_collate_fn else None
-        if dataloader_params.use_custom_batch_sampler:
-            batch_sampler = dataset.batch_sampler(batch_size=dataloader_params.batch_size,
-                                                  shuffle=dataloader_params.shuffle,
-                                                  drop_last=dataloader_params.drop_last)
-            Loader = partial(DataLoader, batch_sampler=batch_sampler)
-        else:
-            Loader = partial(DataLoader, batch_size=dataloader_params.batch_size,
-                             shuffle=dataloader_params.shuffle, drop_last=dataloader_params.drop_last,
-                             pin_memory=dataloader_params.pin_memory)
-        loader = Loader(dataset=dataset,
-                        num_workers=dataloader_params.num_workers,
-                        collate_fn=collate_fn)
-        return loader
-
-    def train_dataloader(self) -> torch.DataLoader:
-        data_params = self.hparams.data
-
-        if data_params.train_params is None:
-            return None
-
-        data_loader = self.prepare_dataloader(data_params.train_params, data_params.common_params,
-                                              data_params.train_params.dataloader_params)
-
+    def train_dataloader(self) -> Optional[List[torch.DataLoader]]:
+        """Implement one or more PyTorch DataLoaders for training."""
+        data_loader = self.__constructor.create_dataloaders('train')
         return data_loader
 
-    def val_dataloader(self) -> torch.DataLoader:
-        data_params = self.hparams.data
-
-        if data_params.valid_params is None:
-            return None
-
-        dataloader_params = data_params.valid_params.dataloader_params.copy()
-        dataloader_params.shuffle = False
-        dataloader_params.drop_last = False
-
-        data_loader = self.prepare_dataloader(data_params.valid_params, data_params.common_params,
-                                              dataloader_params)
-
+    def val_dataloader(self) -> Optional[List[torch.DataLoader]]:
+        """Implement one or multiple PyTorch DataLoaders for prediction."""
+        phase = 'valid'
+        drop_last = self.__hparams['data'][phase][0]['dataloader']['drop_last']
+        shuffle = self.__hparams['data'][phase][0]['dataloader']['shuffle']
+        if shuffle or drop_last:
+            raise ValueError(f'DataLoader parametrs `shuffle` and `drop_last` must be False in {phase} phase.')
+        data_loader = self.__constructor.create_dataloaders(phase)
         return data_loader
 
-    def test_dataloader(self) -> torch.DataLoader:
-        data_params = self.hparams.data
+    def test_dataloader(self) -> Optional[List[torch.DataLoader]]:
+        """Implement one or multiple PyTorch DataLoaders for testing."""
+        phase = 'test'
+        drop_last = self.__hparams['data'][phase][0]['dataloader']['drop_last']
+        shuffle = self.__hparams['data'][phase][0]['dataloader']['shuffle']
+        if shuffle or drop_last:
+            raise ValueError(f'DataLoader parametrs `shuffle` and `drop_last` must be False in {phase} phase.')
+        data_loader = self.__constructor.create_dataloaders(phase)
+        return data_loader
 
-        if data_params.test_params is None:
-            return None
-
-        dataloader_params = data_params.test_params.dataloader_params.copy()
-        dataloader_params.shuffle = False
-        dataloader_params.drop_last = False
-
-        data_loader = self.prepare_dataloader(data_params.test_params, data_params.common_params,
-                                              dataloader_params)
-
+    def predict_dataloader(self) -> Optional[List[torch.DataLoader]]:
+        """Implement one or multiple PyTorch DataLoaders for prediction."""
+        phase = 'predict'
+        drop_last = self.__hparams['data'][phase][0]['dataloader']['drop_last']
+        shuffle = self.__hparams['data'][phase][0]['dataloader']['shuffle']
+        if shuffle or drop_last:
+            raise ValueError(f'DataLoader parametrs `shuffle` and `drop_last` must be False in {phase} phase.')
+        data_loader = self.__constructor.create_dataloaders(phase)
         return data_loader
 
     def training_step_end(self, step_outputs: torch.tensor) -> torch.tensor:
@@ -169,21 +144,21 @@ class BaseTask(LightningModule, ABC):
         return step_outputs.mean(dim=0, keepdim=True)
 
     @property
-    def params(self):
-        return self.__params
+    def hparams(self):
+        """Is hyperparameters that set in yaml file."""
+        return self.__hparams
 
     @property
-    def  metric_manager(self):
-        return self.__metric_manager
+    def metric_manager(self):
+        """Is metric manager."""
+        return self._metric_manager
 
     @property
     def criterion(self):
+        """Is criterion."""
         return self._criterion
 
     @property
     def input_shapes(self):
+        """Is input shape."""
         return self.__input_shapes
-
-    @property
-    def inference_mode(self):
-        return self.__inference_mode
