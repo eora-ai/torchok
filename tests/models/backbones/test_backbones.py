a4056026d8f6730a7b7e0d7ae75ca616c6a2845e
VladislavPatrushev <patr982@gmail.com>
2022-05-16 13:06:49 +0000

    refactor + add convbnact

--- tests/models/backbones/test_backbones.py
+++ tests/models/backbones/test_backbones.py
@@ -1,4 +1,3 @@
-import imp
 import unittest
 
 import timm
@@ -19,8 +18,7 @@ class TestResNet18(unittest.TestCase):
         self.__timm_model(self.__input)
         self.__output['output_last_layer']
 
-        self.__model = BACKBONES.get('resnet18')(pretrained=False, in_chans=3)
-        self.__model.load_state_dict(torch.load("/workdir/vpatrushev/torchOK2/torchok/weights/resnet18-torchok.pth"))
+        self.__model = BACKBONES.get('resnet18')(pretrained=True, in_chans=3)
         self.assertTrue(self.__model(self.__input).equal(self.__output['output_last_layer']))
 
     def get_output(self, name):
@@ -28,25 +26,26 @@ class TestResNet18(unittest.TestCase):
             self.__output[name] = output.detach()
         return hook
 
-# class TestResNet50(unittest.TestCase):
-#     def __init__(self, methodName: str = ...) -> None:
-#         super().__init__(methodName)
-#         self.__input = torch.ones(1, 1, 224, 224)
-#         self.__timm_model = timm.create_model('resnet50', pretrained=True, in_chans=1)
-#         self.__output = {}
 
-#     def test_init(self):
-#         self.__timm_model.layer4[-1].act3.register_forward_hook(self.get_output('output_last_layer'))
-#         self.__timm_model(self.__input)
-#         self.__output['output_last_layer']
+class TestResNet50(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.__input = torch.ones(1, 3, 224, 224)
+        self.__timm_model = timm.create_model('resnet50', pretrained=True)
+        self.__output = {}
 
-#         self.__model = BACKBONES.get('resnet50')(pretrained=True, in_chans=1)
-#         self.assertTrue(self.__model(self.__input).equal(self.__output['output_last_layer']))
+    def test_init(self):
+        self.__timm_model.layer4[-1].act3.register_forward_hook(self.get_output('output_last_layer'))
+        self.__timm_model(self.__input)
+        self.__output['output_last_layer']
 
-#     def get_output(self, name):
-#         def hook(model, input, output):
-#             self.__output[name] = output.detach()
-#         return hook
+        self.__model = BACKBONES.get('resnet50')(pretrained=True, in_chans=3)
+        self.assertTrue(self.__model(self.__input).equal(self.__output['output_last_layer']))
+
+    def get_output(self, name):
+        def hook(model, input, output):
+            self.__output[name] = output.detach()
+        return hook
 
 # class TestSEResNet50(unittest.TestCase):
 #     def __init__(self, methodName: str = ...) -> None:
