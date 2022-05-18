ec4d85c11b419a29fec7c184ce4c80b92ffccf96
VladislavPatrushev <patr982@gmail.com>
2022-05-18 21:14:16 +0000

    after review

--- src/tasks/base.py
+++ src/tasks/base.py
@@ -1,10 +1,11 @@
-from typing import Tuple, List, Optional
+from typing import Any, Dict, Tuple, List, Optional
 from abc import ABC, abstractmethod
 
 import torch
 from pytorch_lightning import LightningModule
 from omegaconf import DictConfig
 
+from src.constructor.config_structure import Phase
 from src.constructor.constuctor import Constructor
 
 
@@ -32,12 +33,12 @@ class BaseTask(LightningModule, ABC):
             self._input_tensors.append(input_tensor)
 
     @abstractmethod
-    def forward(self, *args, **kwargs):
+    def forward(self, *args, **kwargs) -> torch.Tensor:
         """Abstract forward method for validation an test."""
         pass
 
     @abstractmethod
-    def forward_with_gt(batch: dict) -> dict:
+    def forward_with_gt(batch: Dict[str, Any]) -> Dict[str, torch.Tensor]:
         """Abstract forward method for training(with ground truth labels)."""
         pass
 
@@ -49,7 +50,7 @@ class BaseTask(LightningModule, ABC):
         # TODO check and load checkpoint
         pass
 
-    def training_epoch_end(self, outputs: Tuple[torch.tensor, dict]) -> None:
+    def training_epoch_end(self, outputs: Tuple[torch.Tensor, Dict]) -> None:
         """It's calling at the end of the training epoch with the outputs of all training steps."""
         total_loss, tagged_loss_values = outputs
         avg_loss = total_loss.mean()
@@ -59,9 +60,9 @@ class BaseTask(LightningModule, ABC):
             self.log(f'train/{tag}', loss_value, on_step=False, on_epoch=True)
 
         self.log('step', self.current_epoch, on_step=False, on_epoch=True)
-        self.log_dict(self._metrics_manager.on_epoch_end('train'))
+        self.log_dict(self._metrics_manager.on_epoch_end(Phase.TRAIN))
 
-    def validation_epoch_end(self, outputs: Tuple[torch.tensor, dict]) -> None:
+    def validation_epoch_end(self, outputs: Tuple[torch.Tensor, Dict]) -> None:
         """It's calling at the end of the validation epoch with the outputs of all validation steps."""
         total_loss, tagged_loss_values = outputs
         avg_loss = total_loss.mean()
@@ -71,9 +72,9 @@ class BaseTask(LightningModule, ABC):
             self.log(f'valid/{tag}', loss_value, on_step=False, on_epoch=True)
 
         self.log('step', self.current_epoch, on_step=False, on_epoch=True)
-        self.log_dict(self._metrics_manager.on_epoch_end('valid'))
+        self.log_dict(self._metrics_manager.on_epoch_end(Phase.VALID))
 
-    def test_epoch_end(self, outputs: Tuple[torch.tensor, dict]) -> None:
+    def test_epoch_end(self, outputs: Tuple[torch.Tensor, Dict]) -> None:
         """It's calling at the end of a test epoch with the output of all test steps."""
         total_loss, tagged_loss_values = outputs
         avg_loss = total_loss.mean()
@@ -82,7 +83,7 @@ class BaseTask(LightningModule, ABC):
         for tag, loss_value in tagged_loss_values.items():
             self.log(f'test/{tag}', loss_value, on_step=False, on_epoch=True)
 
-        self.log_dict(self._metrics_manager.on_epoch_end('test'))
+        self.log_dict(self._metrics_manager.on_epoch_end(Phase.TEST))
 
     def to_onnx(self, onnx_params) -> None:
         """It's saving the model in ONNX format."""
@@ -117,20 +118,17 @@ class BaseTask(LightningModule, ABC):
 
     def train_dataloader(self) -> Optional[List[torch.DataLoader]]:
         """Implement one or more PyTorch DataLoaders for training."""
-        phase = 'train'
-        data_params = self._hparams['data'].get(phase, None)
+        data_params = self._hparams['data'].get(Phase.TRAIN, None)
 
         if data_params is None:
             return None
 
-        data_loader = self.__constructor.create_dataloaders('train')
+        data_loader = self.__constructor.create_dataloaders(Phase.TRAIN)
         return data_loader
 
     def val_dataloader(self) -> Optional[List[torch.DataLoader]]:
         """Implement one or multiple PyTorch DataLoaders for prediction."""
-        phase = 'valid'
-
-        data_params = self._hparams['data'].get(phase, None)
+        data_params = self._hparams['data'].get(Phase.VALID, None)
 
         if data_params is None:
             return None
@@ -138,16 +136,14 @@ class BaseTask(LightningModule, ABC):
         for data_param in data_params:
             drop_last = data_param['dataloader']['drop_last']
             if drop_last:
-                raise ValueError(f'DataLoader parametrs `drop_last` must be False in {phase} phase.')
+                raise ValueError(f'DataLoader parametrs `drop_last` must be False in {Phase.VALID.value} phase.')
 
-        data_loader = self.__constructor.create_dataloaders(phase)
+        data_loader = self.__constructor.create_dataloaders(Phase.VALID)
         return data_loader
 
     def test_dataloader(self) -> Optional[List[torch.DataLoader]]:
         """Implement one or multiple PyTorch DataLoaders for testing."""
-        phase = 'test'
-
-        data_params = self._hparams['data'].get(phase, None)
+        data_params = self._hparams['data'].get(Phase.TEST, None)
 
         if data_params is None:
             return None
@@ -155,31 +151,37 @@ class BaseTask(LightningModule, ABC):
         for data_param in data_params:
             drop_last = data_param['dataloader']['drop_last']
             if drop_last:
-                raise ValueError(f'DataLoader parametrs `drop_last` must be False in {phase} phase.')
+                raise ValueError(f'DataLoader parametrs `drop_last` must be False in {Phase.TEST.value} phase.')
 
-        data_loader = self.__constructor.create_dataloaders(phase)
+        data_loader = self.__constructor.create_dataloaders(Phase.TEST)
         return data_loader
 
     def predict_dataloader(self) -> Optional[List[torch.DataLoader]]:
         """Implement one or multiple PyTorch DataLoaders for prediction."""
-        phase = 'predict'
-        drop_last = self._hparams['data'][phase][0]['dataloader']['drop_last']
-        if drop_last:
-            raise ValueError(f'DataLoader parametrs `drop_last` must be False in {phase} phase.')
-        data_loader = self.__constructor.create_dataloaders(phase)
+        data_params = self._hparams['data'].get(Phase.PREDICT, None)
+
+        if data_params is None:
+            return None
+
+        for data_param in data_params:
+            drop_last = data_param['dataloader']['drop_last']
+            if drop_last:
+                raise ValueError(f'DataLoader parametrs `drop_last` must be False in {Phase.PREDICT.value} phase.')
+
+        data_loader = self.__constructor.create_dataloaders(Phase.PREDICT)
         return data_loader
 
-    def training_step_end(self, step_outputs: torch.tensor) -> torch.tensor:
+    def training_step_end(self, step_outputs: torch.Tensor) -> torch.Tensor:
         return step_outputs.mean(dim=0, keepdim=True)
 
-    def validation_step_end(self, step_outputs: torch.tensor) -> torch.tensor:
+    def validation_step_end(self, step_outputs: torch.Tensor) -> torch.Tensor:
         return step_outputs.mean(dim=0, keepdim=True)
 
-    def test_step_end(self, step_outputs: torch.tensor) -> torch.tensor:
+    def test_step_end(self, step_outputs: torch.Tensor) -> torch.Tensor:
         return step_outputs.mean(dim=0, keepdim=True)
 
     @property
-    def hparams(self):
+    def hparams(self) -> DictConfig:
         """Hyperparameters that set in yaml file."""
         return self._hparams
 
@@ -194,11 +196,16 @@ class BaseTask(LightningModule, ABC):
         return self._losses
 
     @property
-    def input_shapes(self):
+    def input_shapes(self) -> List[Tuple[int, int, int]]:
         """Input shapes."""
         return self.__input_shapes
 
     @property
-    def input_dtypes(self):
+    def input_dtypes(self) -> List[str]:
         """Input dtypes."""
         return self.__input_dtypes
+
+    @property
+    def input_tensors(self) -> List[torch.Tensor]:
+        """Input tensors."""
+        return self._input_tensors
