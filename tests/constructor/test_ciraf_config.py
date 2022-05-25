5a3c6a527a28d17d2fca7ec62d7cba1fba8cfc7d
Sitcebelly <rashit.bayazitov.1995@gmail.com>
2022-05-25 15:18:08 +0000

    work config structure

---
+++ tests/constructor/test_ciraf_config.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../../')
+
+import hydra
+import omegaconf
+from omegaconf import OmegaConf, DictConfig
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import *
+
+from src.constructor.config_structure import ConfigParams, Phase
+
+
+@hydra.main(config_path='/workdir/rbayazitov/torchok/examples/configs/', config_name='classification_cifar10')
+def main(config: DictConfig):
+    OmegaConf.resolve(config)
+    schema = OmegaConf.structured(ConfigParams)
+    config = OmegaConf.merge(schema, config)
+    params = ConfigParams(**config)
+    return params
+ 
+
+if __name__ == '__main__':
+    params = main()
+    print(params)
