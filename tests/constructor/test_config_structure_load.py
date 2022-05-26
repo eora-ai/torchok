b125c176f6664687439ca409b036167c7b80d0b1
Sitcebelly <rashit.bayazitov.1995@gmail.com>
2022-05-26 09:51:36 +0000

    implement raise test

--- tests/constructor/test_config_structure_load.py
+++ tests/constructor/test_config_structure_load.py
@@ -38,4 +38,7 @@ class TestConfigSrucure(unittest.TestCase):
     def test_optional_type_of_metrics_when_config_does_not_have_metrics(self):
         config = load_structured_config('tests/constructor/configs/config_without_metrics.yaml')
         self.assertEqual(len(config.metrics), 0)
+
+    def test_structure_schema_when_add_not_registrated_parameter(self):
+        self.assertRaises(KeyError, load_structured_config, 'tests/constructor/configs/config_with_bag.yaml')
     
