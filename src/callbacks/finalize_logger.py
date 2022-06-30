ce1eeb33ca8febfe07237f6e31f61f85f80dcdeb
Sitcebelly <rashit.bayazitov.1995@gmail.com>
2022-06-30 17:00:38 +0300

    change status type

--- src/callbacks/finalize_logger.py
+++ src/callbacks/finalize_logger.py
@@ -5,5 +5,5 @@ class FinalizeLogger(Callback):
     """Callback for finalize logger if an error occurs"""
     def on_exception(self, trainer, pl_module, outputs):
         # I think we need to save checkpoints for every exception not even isinstance(outputs, KeyboardInterrupt)
-        status = 'FINISHED' if type(outputs).__name__ == 'KeyboardInterrupt' else 'FAILED'
+        status = 'KILLED' if type(outputs).__name__ == 'KeyboardInterrupt' else 'FAILED'
         trainer.logger.finalize(status)
