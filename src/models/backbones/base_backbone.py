5d4d903e9f52d41aeb7f849bf70bcd58ba52620a
sitcebelly <rashit.bayazitov.1995@gmail.com>
2022-07-01 07:06:53 +0000

    prepare base models classes

---
+++ src/models/backbones/base_backbone.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+from src.models.base import BaseModel
+
+
+class BaseBackbone(BaseModel, ABC):
+    """Base model for all TorchOk Backbone."""
+    def __init__(self):
+        """Initialize BaseModel."""
+        super().__init__()
+
+    @abstractmethod
+    def forward_features(self, *args, **kwargs):
+        """Forward method for getting backbone feature maps.
+           They are mainly used for segmentation and detection tasks.
+        """
+        pass
+
+    @abstractmethod
+    def get_forward_feature_output_channels(self) -> Union[int, List[int]]:
+        """Set output channels for forward features pass.
+        
+        Returns: Outpus channels.
+        """
+        pass
+    
