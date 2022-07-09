a50ddb1b349e6a3eb596a6a6e2040d3576fee468
Sitcebelly <rashit.bayazitov.1995@gmail.com>
2022-07-09 19:22:15 +0300

    refactoring

--- src/callbacks/finalize_logger.py
+++ src/callbacks/finalize_logger.py
@@ -2,7 +2,7 @@ from pytorch_lightning.callbacks import Callback
 
 
 class FinalizeLogger(Callback):
-    """Callback for finalize logger if an error occurs"""
+    """Callback to finalize logger if an error occurs"""
     def on_exception(self, trainer, pl_module, outputs):
         # Need to save checkpoints for every exception not only isinstance(outputs, KeyboardInterrupt)
         status = 'KILLED' if type(outputs) == KeyboardInterrupt else 'FAILED'
