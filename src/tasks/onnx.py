b4f41c5672d33ddb5bd7bc570041fe03fb0ccaf5
VladPatrushev <patr982@gmail.com>
2022-07-15 08:03:18 +0000

    after review

--- src/tasks/onnx.py
+++ src/tasks/onnx.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Any, List
+from typing import Dict, Union, Any
 
 import onnx
 import torch
@@ -34,13 +34,13 @@ class ONNXTask(BaseTask):
         self.__binding = self._sess.io_binding()
 
         self.__input_names = [input_name.name for input_name in self._sess.get_inputs()]
-        self.__input_shapes = [item['shape'] for item in self.__infer_params.inputs]
-
+        self.__input_shapes = [input_param.shape for _, input_param in self.__infer_params.inputs.items()]
+        # batch shape
         dynamic_dim = self.__input_shapes[0][0]
 
-        self.__label_name = self._sess.get_outputs()[0].name
-        self.__output_shape = self._sess.get_outputs()[0].shape
-        self.__dynamic_output_shape = (dynamic_dim, *self.__output_shape[1:])
+        self.__label_names = [label_name.name for label_name in self._sess.get_outputs()]
+        self.__output_shapes = [output_shape.shape for output_shape in self._sess.get_outputs()]
+        self.__dynamic_output_shapes = [(dynamic_dim, *output_shape[1:]) for output_shape in self.__output_shapes]
 
     def forward(self, x: Tensor) -> Tensor:
         pass
@@ -48,13 +48,9 @@ class ONNXTask(BaseTask):
     def forward_with_gt(self, batch: Dict[str, Any]) -> Dict[str, Tensor]:
         pass
 
-    def foward_infer(self, x: Tensor) -> Tensor:
-        """Foward inference."""
-        output = self._forward_onnx(x)
-        return output
-
-    def _forward_onnx(self, inputs: Union[List[Tensor], Tensor]) -> Tensor:
+    def foward_infer(self, inputs: Dict[str, Tensor]) -> Tensor:
         """Forward onnx model."""
+        inputs = [input_tensor for input_name, input_tensor in inputs.items() if input_name in self._input_names]
 
         for input_name, input_shape, input_tensor in zip(self.__input_names, self.__input_shapes, inputs):
             self.__binding.bind_input(
@@ -65,18 +61,24 @@ class ONNXTask(BaseTask):
                 shape=input_shape,
                 buffer_ptr=input_tensor.data_ptr())
 
-        output_tensor = torch.empty(self.__dynamic_output_shape, dtype=torch.float32, device=self.device).contiguous()
+        output = dict()
 
-        self.__binding.bind_output(
-            name=self.__label_name,
-            device_type=self.device,
-            device_id=self.device_id,
-            element_type=np.float32,
-            shape=self.__dynamic_output_shape,
-            buffer_ptr=output_tensor.data_ptr())
+        for label_name, output_shape in zip(self.__label_names, self.__dynamic_output_shapes):
+
+            output_tensor = torch.empty(output_shape, dtype=torch.float32, device=self.device).contiguous()
+
+            self.__binding.bind_output(
+                name=label_name,
+                device_type=self.device,
+                device_id=self.device_id,
+                element_type=np.float32,
+                shape=output_shape,
+                buffer_ptr=output_tensor.data_ptr())
+
+            output[label_name] = output_tensor
 
         self._sess.run_with_iobinding(self.__binding)
-        return output_tensor
+        return output
 
     def forward_infer_with_gt(self, batch: Dict[str, Any]) -> Dict[str, Tensor]:
         """Abstract forward method for test stage."""
@@ -93,5 +95,5 @@ class ONNXTask(BaseTask):
 
     def predict_step(self, batch: Dict[str, Any], batch_idx: int) -> Tensor:
         """Complete predict loop."""
-        output = self.foward_infer(batch['image'])
+        output = self.foward_infer(batch)
         return output
