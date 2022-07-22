fa28907a1c294894ba48f2e5f45e6ab0deee09ff
Vlad Vinogradov <vladt9@yandex.ru>
2022-07-22 11:16:29 +0400

    Uncommented failing tests

--- tests/models/backbones/test_hrnet.py
+++ tests/models/backbones/test_hrnet.py
@@ -1,57 +1,57 @@
-# import unittest
-# from pathlib import Path
+import unittest
+from pathlib import Path
 
-# import torch
-# import torch.nn.functional as F
+import torch
+import torch.nn.functional as F
 
-# from torchok import HEADS, NECKS, BACKBONES
-
-
-# class TestHRNetSegmentation(unittest.TestCase):
-
-#     def __init__(self, backbone_name, methodName: str = ...) -> None:
-#         super().__init__(methodName)
-#         self._onnx_model = Path(__file__).parent / f'{backbone_name}.onnx'
-#         self._input = torch.ones(1, 3, 224, 224)
-#         self.backbone = BACKBONES.get(backbone_name)(pretrained=False)
-#         neck_in_features = self.backbone.out_channels
-#         self.neck = NECKS.get('HRNetSegmentationNeck')(neck_in_features)
-#         head_in_features = self.neck.out_channels
-#         self.head = HEADS.get('HRNetSegmentationHead')(head_in_features, 10)
+from torchok import HEADS, NECKS, BACKBONES
 
 
-# class TestHRNetSegmentation_W18(TestHRNetSegmentation):
+class TestHRNetSegmentation(unittest.TestCase):
 
-#     def __init__(self, methodName: str = ...) -> None:
-#         super().__init__('hrnet_w18', methodName)
-
-#     def test_outputs_equals(self):
-#         backbone_features = self.backbone.forward_features(self._input)
-#         x = self.neck(backbone_features)
-#         x = self.head(x)
-#         self.assertTupleEqual(x.shape, (1, 10, 224, 224))
-
-
-# class TestHRNetClassification(unittest.TestCase):
-
-#     def __init__(self, backbone_name, methodName: str = ...) -> None:
-#         super().__init__(methodName)
-#         self._input = torch.ones(1, 3, 224, 224)
-#         self.backbone = BACKBONES.get(backbone_name)(pretrained=False)
-#         neck_in_features = self.backbone.out_channels
-#         self.neck = NECKS.get('HRNetClassificationNeck')(neck_in_features)
-#         head_in_features = self.neck.out_channels
-#         self.head = HEADS.get('ClassificationHead')(head_in_features, 10)
+    def __init__(self, backbone_name, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self._onnx_model = Path(__file__).parent / f'{backbone_name}.onnx'
+        self._input = torch.ones(1, 3, 224, 224)
+        self.backbone = BACKBONES.get(backbone_name)(pretrained=False)
+        neck_in_features = self.backbone.out_channels
+        self.neck = NECKS.get('HRNetSegmentationNeck')(neck_in_features)
+        head_in_features = self.neck.out_channels
+        self.head = HEADS.get('HRNetSegmentationHead')(head_in_features, 10)
 
 
-# class TestHRNetClassification_W18(TestHRNetClassification):
+class TestHRNetSegmentation_W18(TestHRNetSegmentation):
 
-#     def __init__(self, methodName: str = ...) -> None:
-#         super().__init__('hrnet_w18', methodName)
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__('hrnet_w18', methodName)
 
-#     def test_outputs_equals(self):
-#         x = self.backbone(self._input)
-#         x = self.neck(x)
-#         x = F.avg_pool2d(x, kernel_size=x.size()[2:]).view(x.size(0), -1)
-#         x = self.head(x)
-#         self.assertTupleEqual(x.shape, (1, 10))
+    def test_outputs_equals(self):
+        backbone_features = self.backbone.forward_features(self._input)
+        x = self.neck(backbone_features)
+        x = self.head(x)
+        self.assertTupleEqual(x.shape, (1, 10, 224, 224))
+
+
+class TestHRNetClassification(unittest.TestCase):
+
+    def __init__(self, backbone_name, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self._input = torch.ones(1, 3, 224, 224)
+        self.backbone = BACKBONES.get(backbone_name)(pretrained=False)
+        neck_in_features = self.backbone.out_channels
+        self.neck = NECKS.get('HRNetClassificationNeck')(neck_in_features)
+        head_in_features = self.neck.out_channels
+        self.head = HEADS.get('ClassificationHead')(head_in_features, 10)
+
+
+class TestHRNetClassification_W18(TestHRNetClassification):
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__('hrnet_w18', methodName)
+
+    def test_outputs_equals(self):
+        x = self.backbone(self._input)
+        x = self.neck(x)
+        x = F.avg_pool2d(x, kernel_size=x.size()[2:]).view(x.size(0), -1)
+        x = self.head(x)
+        self.assertTupleEqual(x.shape, (1, 10))
