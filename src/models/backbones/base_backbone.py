9d5f512ecd844865d30560708aedb982c8435181
Animatory <shulz134700@gmail.com>
2022-07-26 11:09:14 +0300

    Add timm resnet

--- src/models/backbones/base_backbone.py
+++ src/models/backbones/base_backbone.py
@@ -2,36 +2,32 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple, Union, Optional
 
 from src.models.base import BaseModel
+from timm.models.features import FeatureHooks
 
 
 class BaseBackbone(BaseModel, ABC):
     """Base model for TorchOk Backbones"""
-    def __init__(self,
-                 in_channels: Optional[Union[int, List[int], Tuple[int, ...]]] = None,
-                 out_channels: Optional[Union[int, List[int], Tuple[int, ...]]] = None,
-                 out_feature_channels: Optional[Union[List[int], Tuple[int, ...]]] = None):
-        """Init BaseBackbone.
 
-        Args:
-            in_channels: Number of input channels.
-            out_features: Number of output channels - channels after forward method.
-            out_feature_channels: Number of output feature channels - channels after forward_features method.
-        """
-        super().__init__(in_channels, out_channels)
-        self._out_feature_channels = out_feature_channels
+    def create_hooks(self):
+        self.stage_names = [i['module'] for i in self.feature_info]
+        self._out_encoder_channels = [i['num_chs'] for i in self.feature_info]
+        hooks = [dict(module=name, type='forward') for name in self.stage_names]
+        self.feature_hooks = FeatureHooks(hooks, self.named_modules())
 
-    @abstractmethod
-    def forward_features(self, *args, **kwargs):
+    def forward_features(self, x):
         """Forward method for getting backbone feature maps.
            They are mainly used for segmentation and detection tasks.
         """
-        pass
+        last_features = self(x)
+        backbone_features = self.feature_hooks.get_output(x.device)
+        backbone_features = list(backbone_features.values())
+        backbone_features = [x] + backbone_features
+        return last_features, backbone_features
 
     @property
-    def out_feature_channels(self) -> List[int]:
+    def out_encoder_channels(self) -> Tuple[int]:
         """Number of output feature channels - channels after forward_features method."""
-        if self._out_feature_channels is None:
+        if self._out_encoder_channels is None:
             raise ValueError('TorchOk Backbones must have self._out_feature_channels attribute.')
-        if isinstance(self._out_feature_channels, tuple):
-            return list(self._out_feature_channels)
-        return self._out_feature_channels
+        return tuple(self._out_encoder_channels)
+
