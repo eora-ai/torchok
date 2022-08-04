8636b45b688bea9cac5742de4783f3e92c39c033
Animatory <shulz134700@gmail.com>
2022-08-04 13:02:09 +0300

    Remove duplicated init_weights

--- torchok/models/necks/segmentation/unet.py
+++ torchok/models/necks/segmentation/unet.py
@@ -129,20 +129,3 @@ class UnetNeck(BaseModel):
             x = decoder_block(x, skip)
 
         return [input_image, x]
-
-    def init_weights(self):
-        for m in self.modules():
-
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_uniform_(m.weight, mode="fan_in", nonlinearity="relu")
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-            elif isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
