196126e56c779cf78386d70db9ea365ee7c34688
root <Sitcebelly>
2022-08-31 07:47:17 +0000

    Implement COCO segmentation dataset

--- torchok/data/datasets/examples/__init__.py
+++ torchok/data/datasets/examples/__init__.py
@@ -2,11 +2,4 @@ from torchok.data.datasets.examples.cifar10 import CIFAR10
 from torchok.data.datasets.examples.sop import SOP
 from torchok.data.datasets.examples.sweet_pepper import SweetPepper
 from torchok.data.datasets.examples.coco_detection import COCODetection
-
-
-__all__ = [
-    'CIFAR10',
-    'SOP',
-    'SweetPepper',
-    'COCODetection',
-]
+from torchok.data.datasets.examples.coco_segmentation import COCOSegmentation
--- torchok/data/datasets/examples/coco_segmentation.py
+++ torchok/data/datasets/examples/coco_segmentation.py
@@ -1,4 +1,6 @@
+import cv2
 import pandas as pd
+import numpy as np
 from pycocotools.coco import COCO
 from pathlib import Path
 from typing import Optional, Union
@@ -8,11 +10,11 @@ from albumentations.core.composition import BaseCompose
 from torchvision.datasets.utils import download_and_extract_archive
 
 from torchok.constructor import DATASETS
-from torchok.data.datasets.detection import DetectionDataset
+from torchok.data.datasets.segmentation import ImageSegmentationDataset
 
 
 @DATASETS.register_class
-class COCODetection(DetectionDataset):
+class COCOSegmentation(ImageSegmentationDataset):
     """A class represent detection COCO dataset https://cocodataset.org/#home.
 
     The COCO Object Detection Task is designed to push the state of the art in object detection forward.
@@ -20,8 +22,8 @@ class COCODetection(DetectionDataset):
     (the latter is also known as instance segmentation).
 
     COCO dataset has 90 categories where 0 - background label.
-    
-    This Dataset loads about 20 minutes and occupies 20 Gb of memory.
+
+    This Dataset load about 28 minutes and occupies 21 Gb of memory.
     """
     base_folder = 'COCO'
 
@@ -37,8 +39,8 @@ class COCODetection(DetectionDataset):
     annotations_url = 'http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
     annotations_hash = 'f4bbac642086de4f52a3fdda2de5fa2c'
 
-    train_pkl = 'train_detection.pkl'
-    valid_pkl = 'valid_detection.pkl'
+    train_csv = 'train_segmentation.csv'
+    valid_csv = 'valid_segmentation.csv'
 
     def __init__(self,
                  train: bool,
@@ -48,11 +50,8 @@ class COCODetection(DetectionDataset):
                  augment: Optional[Union[BasicTransform, BaseCompose]] = None,
                  input_dtype: str = 'float32',
                  target_dtype: str = 'long',
-                 bbox_dtype: str = 'float32',
                  grayscale: bool = False,
                  test_mode: bool = False,
-                 min_area: float = 0.0,
-                 min_visibility: float = 0.0,
                  ):
         """Init SweetPepper.
 
@@ -65,22 +64,9 @@ class COCODetection(DetectionDataset):
             augment: Optional augment to be applied on a sample.
                 This should have the interface of transforms in `albumentations` library.
             input_dtype: Data type of the torch tensors related to the image.
-            target_dtype: Data type of the torch tensors related to the bboxes labels.
-            bbox_dtype: Data type of the torch tensors related to the bboxes.
+            target_dtype: Data type of the torch tensors related to the target mask.
             grayscale: If True, image will be read as grayscale otherwise as RGB.
             test_mode: If True, only image without labels will be returned.
-            bbox_format: Bboxes format, for albumentations transform. Supports the following formats:
-                pascal_voc - [x_min, y_min, x_max, y_max] = [98, 345, 420, 462]
-                albumentations - [x_min, y_min, x_max, y_max] = [0.1531, 0.71875, 0.65625, 0.9625]
-                coco - [x_min, y_min, width, height] = [98, 345, 322, 117]
-                yolo - [x_center, y_center, width, height] = [0.4046875, 0.8614583, 0.503125, 0.24375]
-            min_area: Value in pixels  If the area of a bounding box after augmentation becomes smaller than min_area,
-                Albumentations will drop that box. So the returned list of augmented bounding boxes won't contain
-                that bounding box.
-            min_visibility: Value between 0 and 1. If the ratio of the bounding box area after augmentation to the area
-                of the bounding box before augmentation becomes smaller than min_visibility,
-                Albumentations will drop that box. So if the augmentation process cuts the most of the bounding box,
-                that box won't be present in the returned list of the augmented bounding boxes.
         """
         self.data_folder = Path(data_folder)
         self.path = self.data_folder / self.base_folder
@@ -89,73 +75,72 @@ class COCODetection(DetectionDataset):
             self._download()
 
         # Create train csv
-        train_df_path = self.path / self.train_pkl
+        train_df_path = self.path / self.train_csv
         if not train_df_path.exists():
             train_annotation_path = self.path / 'annotations/instances_train2017.json'
-            train_image_folder = Path('train2017')
-            self.create_annotation(train_annotation_path, train_image_folder, train_df_path)
+            train_mask_save_folder = self.path / 'train2017'
+            self.create_annotation(train_annotation_path, train_mask_save_folder, train_df_path)
 
         # Create valid csv
-        val_df_path = self.path / self.valid_pkl
+        val_df_path = self.path / self.valid_csv
         if not val_df_path.exists():
             val_annotation_path = self.path / 'annotations/instances_val2017.json'
-            val_image_folder = Path('val2017')
-            self.create_annotation(val_annotation_path, val_image_folder, val_df_path)
+            val_mask_save_folder = self.path / 'val2017'
+            self.create_annotation(val_annotation_path, val_mask_save_folder, val_df_path)
 
         if not self.path.is_dir():
             raise RuntimeError('Dataset not found or corrupted. You can use download=True to download it')
 
-        annotation_path = self.train_pkl if train else self.valid_pkl
+        csv_path = self.train_csv if train else self.valid_csv
 
         super().__init__(
             data_folder=self.path,
-            annotation_path=annotation_path,
+            csv_path=csv_path,
             transform=transform,
             augment=augment,
             input_dtype=input_dtype,
             target_dtype=target_dtype,
-            bbox_dtype=bbox_dtype,
             grayscale=grayscale,
             test_mode=test_mode,
-            min_area=min_area,
-            min_visibility=min_visibility
         )
 
-    def create_annotation(self, json_path: str, image_folder: str, save_df_path: str):
+    def create_annotation(self, json_path: str, mask_folder: str, save_df_path: str):
         """Create train-valid csv for loaded COCO dataset.
 
         Args:
             json_path: COCO json annotation file path.
-            image_folder: COCO images folder.
+            mask_folder: COCO mask save folder.
             save_df_path: Pickle save name.
         """
         image_paths = []
-        bboxes = []
-        labels = []
+        mask_paths = []
         coco=COCO(json_path)
         ids = coco.getImgIds()
         for image_id in ids:
             # add image_path
-            file_name = coco.loadImgs(image_id)[0]['file_name']
-            image_paths.append((image_folder / file_name).as_posix())
-            # get bboxes with labels
-            curr_bboxes = []
-            curr_labels = []
+            img_params = coco.loadImgs(image_id)[0]
+            height = img_params['height']
+            width = img_params['width']
+            file_name = img_params['file_name']
+            image_paths.append((mask_folder / file_name).as_posix())
+            # create mask
             annIds = coco.getAnnIds(image_id)
             anns = coco.loadAnns(annIds)
+            mask = np.zeros((height, width), dtype=np.int32)
             for ann in anns:
-                curr_bboxes.append(ann['bbox'])
-                curr_labels.append(ann['category_id'])
-            bboxes.append(curr_bboxes)
-            labels.append(curr_labels)
+                mask += ann['category_id'] * coco.annToMask(ann)
+            # save mask in same folder
+            mask_file_name = 'mask_' + file_name.split('.')[0] + '.png'
+            mask_path = (mask_folder / mask_file_name).as_posix()
+            mask_paths.append(mask_path)
+            cv2.imwrite(mask_path, mask)
 
         df = pd.DataFrame()
         df['image_path'] = image_paths
-        df['bbox'] = bboxes
-        df['label'] = labels
+        df['mask_path'] = mask_paths
 
-        # Save pickle
-        df.to_pickle(save_df_path)
+        # Save csv
+        df.to_csv(save_df_path, index=False)
 
     def _download(self) -> None:
         """Download archive by url to specific folder."""
