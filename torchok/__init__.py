24e5256194363e54c7a47189fb91303c874298fb
VladPatrushev <patr982@gmail.com>
2022-09-01 10:24:50 +0000

    del noqa in torchok/__init__

--- torchok/__init__.py
+++ torchok/__init__.py
@@ -1,9 +1,9 @@
-from torchok import constructor  # noqa: F401
-from torchok import data  # noqa: F401
-from torchok import losses  # noqa: F401
-from torchok import metrics  # noqa: F401
-from torchok import models  # noqa: F401
-from torchok import optim  # noqa: F401
-from torchok import tasks  # noqa: F401
-from torchok.constructor import (BACKBONES, DATASETS, HEADS, LOSSES, METRICS, NECKS,  # noqa: F401
+from torchok import constructor
+from torchok import data
+from torchok import losses
+from torchok import metrics
+from torchok import models
+from torchok import optim
+from torchok import tasks
+from torchok.constructor import (BACKBONES, DATASETS, HEADS, LOSSES, METRICS, NECKS,
                                  OPTIMIZERS, POOLINGS, SCHEDULERS, TASKS, TRANSFORMS)
