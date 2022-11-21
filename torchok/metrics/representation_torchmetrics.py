7e2f7dc44950540758a0a73b1bf7f02861d5a1cc
Sitcebelly <sitcebelly@optic.xyz>
2022-11-21 16:46:04 +0000

    add ability to choice if use batch searching or no

--- torchok/metrics/representation_torchmetrics.py
+++ torchok/metrics/representation_torchmetrics.py
@@ -41,7 +41,10 @@ class TorchMetricBaseMetr(IndexBasedMeter):
         preds = torch.tensor(closest_scores)
         target = torch.tensor([np.isin(closest_idxs[i], relevants_idxs[i]) for i in range(len(closest_idxs))],
                               dtype=torch.long)
-        indexes = torch.tensor([target.shape[1] * [i] for i in range(len(target))], dtype=torch.long)
+        if self.use_batching_search:
+            indexes = torch.tensor([target.shape[1] * [i] for i in range(len(target))], dtype=torch.long)
+        else:
+            indexes = torch.zeros_like(target, dtype=torch.long)
         return [preds, target, indexes]
 
 
